b701afdb42548e1b34a792141fe9eb5f40a9fac9
SurajGupta07 <bgs071998@gmail.com>
2021-09-22 13:55:44 +0530

    Feat: Create new post

--- src/features/post/pages/CreateNewPost.jsx
+++ src/features/post/pages/CreateNewPost.jsx
@@ -1,10 +1,43 @@
-import { Button } from "../../../common/components/Button";
+import { useState } from "react";
 
 export const CreateNewPost = () => {
+    let [content, setContent] = useState("");
+    const [createPost, setCreatePost] = useState(false);
+    const [input, setShowInput] = useState(true);
+
     return (
         <div>
-            <Button />
-            Create new post
+            {createPost && (
+                <div>
+                    <div className="p-2 text-lg inline-flex">
+                        What's happening?
+                    </div>
+                    <button
+                        onClick={() => {
+                            setCreatePost(false);
+                            setShowInput(true);
+                        }}
+                    >
+                        Close
+                    </button>
+                    <div>
+                        <textarea
+                            style={{ resize: "none" }}
+                            onChange={(e) => setContent(e.target.value)}
+                        ></textarea>
+                        <div>{content.length}/100</div>
+                    </div>
+                    <div></div>
+                </div>
+            )}
+            {input && (
+                <input
+                    onFocus={() => {
+                        setCreatePost(true);
+                        setShowInput(false);
+                    }}
+                ></input>
+            )}
         </div>
-    )
-}
+    );
+};
